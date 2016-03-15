fbfa8f9cee25ea292ee39b5010fe8e4da5ab6f31
Ben Roa <benihanas1@gmail.com>
2016-03-14 22:53:26 -0700

    "initial unworking attempt."

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,39 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+    if(typeof obj === 'number' || typeof obj ==='boolean'){
+        return obj.toString();
+    }
+    else if(typeof obj === 'string'){
+        return '"' + obj + '"';
+    }
+    else if(obj === null && typeof obj === "object"){
+        return "null";
+    }
+    else if(typeof obj === undefined){
+        return undefined;
+    }
+    else if(typeof obj === 'function'){
+        return undefined;
+    }
+    else if(Array.isArray(obj)){
+        var json = [];
+        obj.forEach(function(value){
+            json.push(stringifyJSON(value));
+        });
+        return '[' + json.join(',') + ']';
+    }
+    else if({}.toString.call(obj) === '[object Object]'){
+        var keyval = [];
+        for(var key in obj){
+            if(key !== undefined && obj[key] !== undefined){
+                keyval.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+            }else{
+                return '{}';
+            }   
+        }
+        return '{' + keyval.join(',') + '}';
+    }
 };
