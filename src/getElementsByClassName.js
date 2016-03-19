86d825f4f94df02f539010824093a8eb424657cd
Ben Roa <benihanas1@gmail.com>
2016-03-19 13:25:28 -0700

    "adding initial attempt"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+
+  function getclass(node, results, className) {
+    var nodes = node.childNodes;
+    for (var i = 0; i < nodes.length; i++) {
+      var classes = nodes[i].classList;
+      if(classes && classes.contains(className)){
+      	results.push(nodes[i]);
+      }
+      if(nodes[i].childNodes[0]){
+     		getclass(nodes[i], results, className);
+      }
+      //console.log(classes);
+    }
+  }
+  getclass(document, results, className);
+
+	//console.log(results);
+  return results;
 };
+
+getElementsByClassName('Menu');
+
+//document.body, element.childNodes, and element.classList
